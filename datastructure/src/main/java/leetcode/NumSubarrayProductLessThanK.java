5d0b5e1d663089ee5159b30696d0957a5397790d
Blanchedidngding <18321716610@163.com>
2018-08-06 23:53:42 +0800

    add

--- datastructure/src/main/java/leetcode/NumSubarrayProductLessThanK.java
+++ datastructure/src/main/java/leetcode/NumSubarrayProductLessThanK.java
@@ -18,25 +18,63 @@ import java.util.List;
  * Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
  */
 public class NumSubarrayProductLessThanK {
-    int count;
 
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-       for(int i = 1; i <= nums.length; i++){
-            helper(i, nums, k, 1,0 );
-       }
+        if(nums.length < 0) return 0;
+        int begin = 0, count = 0,end = 0, mul = 1;
+        for(;end < nums.length; end ++){
+            mul *= nums[end];
+            while(begin <= end && mul >= k){
+                mul /= nums[begin];
+                begin ++;
+            }
+            count += end - begin + 1;
+        }
+        return count;
     }
 
-    public void helper(int leftNum, int[] nums, int k, int mul, int beginIndex){
-       if(leftNum == 0) {
-           if(mul < k) count ++;
-       } else {
-           for(int j = beginIndex; j < nums.length; j++){
-               if(mul * nums[j] < k ) count++;
-               else break;
-           }
-           for()
-       }
+
+    public int numSubarrayProductLessThanK2(int[] nums, int k) {
+        int begin = 0, count = 0,end = 0, mul = 1;
+        if(nums.length < 0) return 0;
+        while(begin < nums.length){
+            System.out.println("begin:" + begin + " end:" + end);
+            if(end >= nums.length){
+                System.out.println("end:" + end + " end >= nums.length");
+                begin ++;
+                end = begin;
+                mul = 1;
+                System.out.println("mpdify:" + "end=begin=" + begin);
+                System.out.println("continue...");
+                continue;
+            }
+            mul *= nums[end];
+            System.out.println("mul=" + mul);
+            if(mul < k){
+                count ++;
+                end ++;
+                System.out.println("mul<k: " + " count=" + count + " end=" + end);
+            } else {
+                mul /= nums[begin];
+                if(mul < k){
+                    count += Math.pow(2,end - begin)-1;
+                    begin++;
+                    end ++;
+
+                    System.out.println("mul/begin < k: count+" + (Math.pow(2,end - begin)-1) + "=" + count);
+                    System.out.println("begin:" + begin + " end:" + end);
+                } else {
+                    mul = 1;
+                    begin ++;
+                    end = begin;
+                    System.out.println("mul>=k: " + " mul=1 begin=end=" + end);
+                }
+
+            }
+        }
+        return count;
     }
+
 
     public static void main(String[] args) {
         NumSubarrayProductLessThanK n = new NumSubarrayProductLessThanK();
